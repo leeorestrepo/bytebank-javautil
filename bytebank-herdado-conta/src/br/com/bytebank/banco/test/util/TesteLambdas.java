5a675837efe8e4e969130f4db15f99dbe207ea04
Leonardo Restrepo <leorpo@hotmail.com>
2023-01-23 11:13:43 -0300

    Alura - Java.util: Coleções, Wrappers e Lambda

---
+++ bytebank-herdado-conta/src/br/com/bytebank/banco/test/util/TesteLambdas.java
@@ -0,0 +1,68 @@
+package br.com.bytebank.banco.test.util;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.Iterator;
+import java.util.List;
+import java.util.function.Consumer;
+
+import br.com.bytebank.banco.modelo.Cliente;
+import br.com.bytebank.banco.modelo.Conta;
+import br.com.bytebank.banco.modelo.ContaCorrente;
+import br.com.bytebank.banco.modelo.ContaPoupanca;
+
+public class TesteLambdas {
+
+	public static void main(String[] args) {
+
+		Conta cc1 = new ContaCorrente(22, 33);
+		Cliente clienteCC1 = new Cliente();
+		clienteCC1.setNome("Leonardo");
+		cc1.setTitular(clienteCC1);
+		cc1.deposita(333.0);
+
+		Conta cc2 = new ContaPoupanca(22, 44);
+		Cliente clienteCC2 = new Cliente();
+		clienteCC2.setNome("Denis");
+		cc2.setTitular(clienteCC2);
+		cc2.deposita(444.0);
+
+		Conta cc3 = new ContaCorrente(22, 11);
+		Cliente clienteCC3 = new Cliente();
+		clienteCC3.setNome("Samuel");
+		cc3.setTitular(clienteCC3);
+		cc3.deposita(111.0);
+
+		Conta cc4 = new ContaPoupanca(22, 22);
+		Cliente clienteCC4 = new Cliente();
+		clienteCC4.setNome("Adriano");
+		cc4.setTitular(clienteCC4);
+		cc4.deposita(222.0);
+
+		List<Conta> lista = new ArrayList<>();
+		lista.add(cc1);
+		lista.add(cc2);
+		lista.add(cc3);
+		lista.add(cc4);
+
+		// Functions Object
+		lista.sort(
+				(Conta c1, Conta c2) -> Integer.compare(c1.getNumero(), c2.getNumero())
+		);
+
+		Comparator<Conta> comp = (Conta c1, Conta c2) -> {
+			String nomeC1 = c1.getTitular().getNome();
+			String nomeC2 = c2.getTitular().getNome();
+			return nomeC1.compareTo(nomeC2);
+		};
+		
+//		lista.forEach( (conta) -> System.out.println(conta + ", " + conta.getTitular().getNome()) );
+		Iterator<Conta> it = lista.iterator();
+		
+		while(it.hasNext()) {
+			System.out.println(it.next());
+		}
+		
+	}
+
+}
